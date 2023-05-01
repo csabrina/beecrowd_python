b44bde7f3d251cb226b187d704c834de31880264
Sabrina Costa <ccosta.sabrina@gmail.com>
2023-05-01 19:47:15 -0300

    mais questoes resolvidas

--- 1045.py
+++ 1045.py
@@ -6,15 +6,15 @@ B = ordenar[1]
 C = ordenar[0]
 
 if A >= (B + C):
-    print('NAO FORMA TRIANGULO')
+    print("NAO FORMA TRIANGULO")
 else:
     if A**2 == (B**2 + C**2):
-        print('TRIANGULO RETANGULO')
+        print("TRIANGULO RETANGULO")
     if A**2 > (B**2 + C**2):
-        print('TRIANGULO OBTUSANGULO')
+        print("TRIANGULO OBTUSANGULO")
     if A**2 < (B**2 + C**2):
-        print('TRIANGULO ACUTANGULO')
+        print("TRIANGULO ACUTANGULO")
     if A == B == C:
-        print('TRIANGULO EQUILATERO')
+        print("TRIANGULO EQUILATERO")
     if A == B != C or A == C != B or B == C != A:
-        print('TRIANGULO ISOSCELES')
+        print("TRIANGULO ISOSCELES")
